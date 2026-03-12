7a195e8220a6eaddfb3d3c999beb40cc9a063962
Chintu <yugandharpitta5@gmail.com>
2026-03-13 00:09:21 +0530

    Prepare server.js for Render deployment

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config(); // for environment variables
 
 const app = express();
 app.use(cors());
@@ -11,82 +11,55 @@ app.use(express.json());
 
 const server = http.createServer(app);
 
-const io = new Server(server,{
-  cors:{
-    origin:"http://localhost:3000",
-    methods:["GET","POST"]
+const io = new Server(server, {
+  cors: {
+    origin: process.env.chat-pulse.netlify.app, // set in Render environment
+    methods: ["GET", "POST"]
   }
 });
 
 /* MongoDB */
-
-mongoose.connect("mongodb://127.0.0.1:27017/chatapp")
-.then(()=>console.log("MongoDB Connected"))
-.catch(err=>console.log(err));
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 /* Message Model */
-
-const Message = mongoose.model("Message",{
-  user:String,
-  msg:String
+const Message = mongoose.model("Message", {
+  user: String,
+  msg: String
 });
 
 /* Users */
-
 let users = [];
 
 /* Socket */
-
-io.on("connection", async (socket)=>{
-
+io.on("connection", async (socket) => {
   console.log("User connected");
 
   const messages = await Message.find();
-  socket.emit("load_messages",messages);
+  socket.emit("load_messages", messages);
 
-  /* NEW USER */
-
-  socket.on("new_user",(username)=>{
-
-    if(!users.includes(username)){
-      users.push(username);
-    }
-
-    io.emit("user_list",users);
-
+  socket.on("new_user", (username) => {
+    if (!users.includes(username)) users.push(username);
+    io.emit("user_list", users);
   });
 
-  /* SEND MESSAGE */
-
-  socket.on("send_message", async (data)=>{
-
-    const message = new Message({
-      user:data.user,
-      msg:data.msg
-    });
-
+  socket.on("send_message", async (data) => {
+    const message = new Message({ user: data.user, msg: data.msg });
     await message.save();
-
-    io.emit("receive_message",data);
-
+    io.emit("receive_message", data);
   });
 
-  /* DELETE USER */
-
-  socket.on("delete_user",(username)=>{
-
+  socket.on("delete_user", (username) => {
     users = users.filter(u => u !== username);
-
-    io.emit("user_list",users);
-
+    io.emit("user_list", users);
   });
 
-  socket.on("disconnect",()=>{
+  socket.on("disconnect", () => {
     console.log("User disconnected");
   });
-
 });
 
-server.listen(5000,()=>{
-  console.log("Server running on port 5000");
-});
+/* Start server on dynamic port */
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
